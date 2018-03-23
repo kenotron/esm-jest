a61156c7a3e98de301cffdbcc22c817a172e26f4
John-David Dalton <john.david.dalton@gmail.com>
2018-03-23 13:43:48 -0700

    Simplify usage example.

--- esm-jest.js
+++ esm-jest.js
@@ -5,45 +5,61 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+"use strict";
 
-const jestRuntime = require('jest-runtime');
 const fs = require('fs');
+const jestRuntime = require('jest-runtime');
+const loader = require('esm')(module);
+const path = require('path');
+
+const api = [
+  "afterAll",
+  "afterEach",
+  "beforeAll",
+  "beforeEach",
+  "describe",
+  "expect",
+  "it",
+  "test"
+];
+
 const oldRequireModule = jestRuntime.prototype.requireModule;
 
-function isEsm(module) {
-    if (module.indexOf('.mjs') > -1) {
-        return true;
-    }
+jestRuntime.prototype.requireModule = function (from, moduleName, options) {
+  const modulePath = this._resolveModule(from, moduleName);
+  const ext = path.extname(modulePath);
 
-    const contents = fs.readFileSync(module).toString();
+  if (this._resolver.isCoreModule(moduleName) ||
+      (options && options.isInternalModule) ||
+      (ext === ".json" || ext === ".node")) {
+    return Reflect.apply(oldRequireModule, this, [from, moduleName, options]);
+  }
 
-    if (contents.match(/\n\s*(import|export)\b/) || contents.match(/^\s*(import|export)\b/)) {
-        return true;
-    }
+  const moduleRegistry = this._moduleRegistry;
 
-    return false;
-}
+  if (moduleRegistry[modulePath]) {
+    return moduleRegistry[modulePath].exports;
+  }
 
-jestRuntime.prototype.requireModule = function(from, moduleName, options) {
-    if (options && options.isInternalModule) {
-        return oldRequireModule.apply(this, [from, moduleName, options]);
-    }
+  const localModule = {
+    children: [],
+    exports: {},
+    filename: modulePath,
+    id: modulePath,
+    loaded: false
+  };
 
-    if (isEsm(from)) {
-        const environment = this._environment;
-        Object.assign(global, {
-            describe: environment.global.describe,
-            it: environment.global.it,
-            test: environment.global.test,
-            expect: environment.global.expect,
-            beforeAll: environment.global.beforeAll,
-            beforeEach: environment.global.beforeEach,
-            afterAll: environment.global.afterAll,
-            afterEach: environment.global.afterEach,
-        });
-        module.exports = require('esm')(module)(from, { mode: 'auto', cjs: true });
-        return module;
-    }
+  const envGlobal = this._environment.global;
+
+  api.forEach((name) => {
+    global[name] = envGlobal[name];
+  });
+
+  moduleRegistry[modulePath] = localModule;
+
+  localModule.exports = loader(from);
+  localModule.loaded = true;
+  return localModule;
 };
 
-cli = require('jest/bin/jest');
+require('jest/bin/jest');
